d2eb079293bc2060633444f54f0f7aa15baadc9b
Jessica Leach <jessicamaryleach@gmail.com>
2016-11-03 16:34:02 +0100

    like button added

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const userRoutes = require('./routes/users'),
 var app = express();
 		displayUsers = require('./user-store');
 
+var likeStore = JSON.parse(fs.readFileSync('likes.json'));
+
 app.set('view engine', 'pug');
+
+app.use(express.static('./public'));
 
 app.use(morgan('dev'));
 
@@ -32,7 +36,19 @@ app.get('/', (request, response) => {
 	response.render('search/user', { users: displayUsers.searchUsers() });
 });
 
+app.post('/like', (request, response) => {
+  likeStore.likeCount = likeStore.likeCount + 1;
 
+  response.json(likeStore);
+
+  fs.writeFile('likes.json', JSON.stringify(likeStore), (error, data) => {
+    if (error) {
+      throw error;
+    }
+
+    console.log('new likeCount added to likes.json');
+  });
+});
 
 app.listen(3001, function() {
  console.log('Web server started on port 3001');
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,8 @@
 $('.jl-like-button').on('click', function() {
   $.post('/like', function(data) {
+    $('.jl-like-button').text('LIKES: ' + data.likeCount);
+    console.log(data.likeCount);
     console.log('like finsihed');
+
   });
 });
